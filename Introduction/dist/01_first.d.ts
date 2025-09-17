33dbf45d490c46b7f67a4a910c37d6173da2f191
sumanitian <sumanprasad26042001@gmail.com>
2025-09-17 22:18:50 +0530

    initialization of file and folder

---
+++ Introduction/dist/01_first.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=01_first.d.ts.map
---
+++ Introduction/dist/01_first.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Declare a variable 'x' and initialize it with a numeric value
+let x = 10; // Type inference applied
+// Output the value of 'x' to the console
+console.log(x);
+// Attempt to reassign 'x' with a string value 
+// x = "Prasad"; // Type error beacuse in above we specified x to be a number 
+// Declare a variable 'fullName' and assign a string value to it
+let fullName = "Suman Prasad";
+// Display the value of 'fullName' in the console
+console.log(fullName);
+//# sourceMappingURL=01_first.js.map
